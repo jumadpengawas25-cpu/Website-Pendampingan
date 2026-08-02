fc1b107847116aca57670344dd220342b1fa5aa3
Jumad <jumadpengawas25@gmail.com>
2026-08-03 00:19:39 +0700

    style: mengubah layout grid sekolah binaan menjadi 3 kolom (3-3-3)

--- landing_page_pengawas_sma/src/components/SchoolsSection.jsx
+++ landing_page_pengawas_sma/src/components/SchoolsSection.jsx
@@ -19,7 +19,7 @@ export default function SchoolsSection() {
           </button>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-gutter">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-gutter">
           {schools.map((school) => (
             <SchoolCard key={school.id} school={school} />
           ))}
