33465b395bfc63c1f80ba88715f9832975312640
Jumad <jumadpengawas25@gmail.com>
2026-08-03 14:17:15 +0700

    Update Header.jsx

--- landing_page_pengawas_sma/src/components/Header.jsx
+++ landing_page_pengawas_sma/src/components/Header.jsx
@@ -191,7 +191,7 @@ export default function Header() {
         <TopBar />
         <header className="bg-white shadow-md">
       <nav>
-        <div className="flex items-center justify-between w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
           <Link to="/" className="flex items-center">
             <img
               src={logoWebBaru}
